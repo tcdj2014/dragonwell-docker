1fe8eff677d8d9964deda5a1c9b78fb26512123c
汤星明 <xmtang@ittx.com.cn>
2024-09-14 16:51:43 +0800

    dragonwell action

--- 8/source.sh
+++ 8/source.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sed -i 's/mirrors.cloud.aliyuncs.com/mirrors.aliyun.com/g' alinux3-module.repo
-sed -i 's/mirrors.cloud.aliyuncs.com/mirrors.aliyun.com/g' alinux3-os.repo
-sed -i 's/mirrors.cloud.aliyuncs.com/mirrors.aliyun.com/g' alinux3-plus.repo
-sed -i 's/mirrors.cloud.aliyuncs.com/mirrors.aliyun.com/g' alinux3-powertools.repo
-sed -i 's/mirrors.cloud.aliyuncs.com/mirrors.aliyun.com/g' alinux3-updates.repo
-sed -i 's/mirrors.cloud.aliyuncs.com/mirrors.aliyun.com/g' epel.repo
+sed -i 's/mirrors.cloud.aliyuncs.com/mirrors.aliyun.com/g' /etc/yum.repos.d/alinux3-module.repo
+sed -i 's/mirrors.cloud.aliyuncs.com/mirrors.aliyun.com/g' /etc/yum.repos.d/alinux3-os.repo
+sed -i 's/mirrors.cloud.aliyuncs.com/mirrors.aliyun.com/g' /etc/yum.repos.d/alinux3-plus.repo
+sed -i 's/mirrors.cloud.aliyuncs.com/mirrors.aliyun.com/g' /etc/yum.repos.d/alinux3-powertools.repo
+sed -i 's/mirrors.cloud.aliyuncs.com/mirrors.aliyun.com/g' /etc/yum.repos.d/alinux3-updates.repo
+sed -i 's/mirrors.cloud.aliyuncs.com/mirrors.aliyun.com/g' /etc/yum.repos.d/epel.repo
 
 if [ "$(uname -m)" = "x86_64" ]; then
   echo "宿主机x86_64"
